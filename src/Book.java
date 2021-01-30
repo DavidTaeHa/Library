faef4287611b63167b0fa2e7e4819795ef808e09
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-01-30 17:13:35 -0500

    Added methods for Book class

--- src/Book.java
+++ src/Book.java
@@ -4,4 +4,28 @@
  *
  */
 public class Book {
+    private String number; //a 5-digit serial number unique to the book
+    private String name;
+    private boolean checkedOut;
+    private Date datePublished;
+
+    /**
+     * Compares two books to see if they are equal
+     * @param obj which represents another book to compare
+     * @return boolean value true if the serial numbers for two books are the same
+     */
+    @Override
+    public boolean equals(Object obj){
+
+    }
+
+    /**
+     * Prints out a textual representation of a book in this format:
+     * Book Number::Book Name::Date Published::Availability.
+     * @return text representation of book
+     */
+    @Override
+    public String toString() {
+
+    }
 }
