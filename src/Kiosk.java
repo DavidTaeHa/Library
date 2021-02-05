1af2b6cab9ebda2ad959c4efa72ca2f708be3af4
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-02-05 11:17:49 -0500

    Update Kiosk.java

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -7,7 +7,14 @@ import java.util.StringTokenizer;
  * @author David Ha, Andrew McAvoy
  */
 public class Kiosk {
-    private boolean kioskSession = true;
+    private boolean kioskSession;
+
+    /**
+     * Constructor for kiosk class
+     */
+    public Kiosk(){
+        this.kioskSession = true;
+    }
 
     /**
      * Handles all user input and output
@@ -21,7 +28,24 @@ public class Kiosk {
             input = scan.next();
             String condition = tokenizer.nextToken();
             switch(condition){
-
+                case "PA":
+                    //print books with current sequence
+                case "PD":
+                    //print books by date published
+                case "PN":
+                    //print books by numbers
+                case "A":
+                    //add book
+                case "R":
+                    //remove book
+                case "O":
+                    //checkout book
+                case "I":
+                    //return book
+                case "Q":
+                    kioskSession = false;
+                default:
+                    System.out.println("Invalid Command!");
             }
         }
     }
